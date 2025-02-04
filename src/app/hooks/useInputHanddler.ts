88d3fe2b0a7fda0d910ca876ed1dfd9757618c08
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2025-02-04 21:34:08 +0530

    url changed

--- src/app/hooks/useInputHanddler.ts
+++ src/app/hooks/useInputHanddler.ts
@@ -54,7 +54,7 @@ const {setTaskdata,Taskdata}= useTaskStore()
     //   const result = await db.collection<Task>('tasks').insertOne(task);
   
    
-    const response = await fetch("/", {
+    const response = await fetch("/api", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -66,7 +66,7 @@ const {setTaskdata,Taskdata}= useTaskStore()
         throw new Error("Failed to add task");
       }
 
-      const res1 = await fetch('/');
+      const res1 = await fetch('/api');
       if (!res1.ok) {
         throw new Error("Failed to fetch updated tasks");
       }
